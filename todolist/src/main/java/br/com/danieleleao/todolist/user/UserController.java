bc76bacfd9b192885d9c85daaf562018306fb463
MatheusGomes notebook <matheus.recin@outlook.com>
2023-10-13 15:04:21 -0300

    Sexta_13-10-23_15:04

--- todolist/src/main/java/br/com/danieleleao/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/danieleleao/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 /**
  * Modificadores:
  * public
@@ -41,7 +43,13 @@ public class UserController {  // o nome da classe tem que ser igual ao nome do 
             // status code... se foi sucesso ou erro na operação
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
+
+        // faz a criptografia da senha do usuário assim que ele é gerado
+        var passwordHashred = BCrypt.withDefaults()
+        .hashToString(12, userModel.getPassword().toCharArray());
         
+        userModel.setPassword(passwordHashred);
+
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
